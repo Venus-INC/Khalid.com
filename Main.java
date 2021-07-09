08cbe7d1d1f6e02a9dfc9c5558543ebb68898f9c
Venus-LLC <86935164+Venus-LLC@users.noreply.github.com>
2021-07-09 15:01:30 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public static void (var <String> arg[]);{}
+this.Window.x = 167;
+this.Window.y = 564;
+public class Window{
+const window.Text<String> = "Test";
+  System.out.Printin(window.Text);
+}
